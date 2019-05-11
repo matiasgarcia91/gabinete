20e97c5ef19d10c93c87b515a962c47c75d93dc3
Matias Garcia <matiasigarcia91@gmail.com>
2019-05-11 14:07:01 +0200

    Add text fields and button to product page

--- client/src/components/product-page/ProductPage.jsx
+++ client/src/components/product-page/ProductPage.jsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from "react";
 import { withRouter } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 import HalfHeader from "../half-header/HalfHeader";
 import testImg from "../shop-page/a.jpg";
@@ -23,6 +24,23 @@ const styles = theme => ({
   page: {
     marginRight: "20px",
     marginLeft: "20px"
+  },
+  button: {
+    margin: theme.spacing.unit,
+    backgroundColor: "#282828",
+    width: 200,
+    borderRadius: "2px",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#282828"
+    }
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-end",
+    marginTop: 40,
+    paddingRight: 100
   }
 });
 
@@ -44,11 +62,34 @@ class ProductPage extends PureComponent {
                 <img src={testImg} alt={""} className={classes.images} />
               </div>
             </Grid>
+            <Grid item xs={6} className={classes.buttonContainer}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignContent: "flex-start",
+                  width: 200
+                }}
+              >
+                <div>Title and button</div>
+                <div>Price</div>
+              </div>
+              <Button
+                variant="contained"
+                className={classes.button}
+                disableRipple
+                disableFocusRipple
+              >
+                ADD TO CART
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <div>Description</div>
+            </Grid>
           </Grid>
         </div>
       </div>
     );
   }
 }
-
 export default withRouter(withStyles(styles)(ProductPage));
